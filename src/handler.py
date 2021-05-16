85547eedf58df4bebd39619df9c3610f72e57645
Aniket Goel <aniket.goel@tothenew.com>
2021-05-16 14:19:00 +0530

    add print statement

--- src/handler.py
+++ src/handler.py
@@ -5,6 +5,7 @@ def hello(event, context):
     a = np.arange(15).reshape(3, 5)
     print("Your numpy array:")
     print(a)
+    print("completed!!")
 
 if __name__ == "__main__":
     hello('', '')
